91afcd94834906f03b78afecee9c014ca88c0645
ollinaie <ollinaie@gmx.net>
2011-01-08 20:06:30 +0100

    pretty printed (and tested):
    load("steal/rhino/steal.js");
    steal.plugins('steal/clean',function(){
    	steal.clean('funcunit/syn/qunit.html',{
    		indent_size: 1,
    		indent_char: '\t',
    		jslint : false,
    		ignore: /jquery\/jquery.js|funcunit\/syn\/resources\/jquery.js/,
    		predefined: {
    			steal: true,
    			jQuery: true,
    			$ : true,
    			window : true
    		}
    	});
    }).plugins('steal/clean',function(){
    	steal.clean('test.html',{
    		indent_size: 1,
    		indent_char: '\t',
    		jslint : false,
    		ignore: /jquery\/jquery.js|funcunit\/syn\/resources\/jquery.js/,
    		predefined: {
    			steal: true,
    			jQuery: true,
    			$ : true,
    			window : true
    			}
    	});
    });

--- test/qunit/syn_test.js
+++ test/qunit/syn_test.js
@@ -5,8 +5,7 @@ st = {
 		return document.getElementById(id)
 	},
 	log: function( c ) {
-		if(st.g("mlog"))
-			st.g("mlog").innerHTML = st.g("mlog").innerHTML+c+"<br/>"
+		if ( st.g("mlog") ) st.g("mlog").innerHTML = st.g("mlog").innerHTML + c + "<br/>"
 	},
 	binder: function( id, ev, f ) {
 		st.bind(st.g(id), ev, f)
@@ -14,36 +13,42 @@ st = {
 	unbinder: function( id, ev, f ) {
 		st.unbind(st.g(id), ev, f)
 	},
-	bind : window.addEventListener ? 
-			function(el, ev, f){el.addEventListener(ev, f, false)} : 
-			function(el, ev, f){ el.attachEvent("on"+ev, f) },
-	unbind : window.addEventListener ?
-			function(el, ev, f){ el.removeEventListener(ev, f, false)} :
-			function(el, ev, f){ el.detachEvent("on"+ev, f) }
+	bind: window.addEventListener ?
+	function( el, ev, f ) {
+		el.addEventListener(ev, f, false)
+	} : function( el, ev, f ) {
+		el.attachEvent("on" + ev, f)
+	},
+	unbind: window.addEventListener ?
+	function( el, ev, f ) {
+		el.removeEventListener(ev, f, false)
+	} : function( el, ev, f ) {
+		el.detachEvent("on" + ev, f)
+	}
 };
 
 
-(function(){
-	for(var name in Syn.support){
-		st.log(name+": "+Syn.support[name])
+(function() {
+	for ( var name in Syn.support ) {
+		st.log(name + ": " + Syn.support[name])
 	}
 })();
 
-test("Selecting a select element", function(){
-	st.g("qunit-test-area").innerHTML = 
-		"<form id='outer'><select name='select'><option value='1' id='one'>one</option><option value='2' id='two'>two</option></select></form>";
-	
-	var change = 0, changef = function(){
-		change++;
-	}
+test("Selecting a select element", function() {
+	st.g("qunit-test-area").innerHTML = "<form id='outer'><select name='select'><option value='1' id='one'>one</option><option value='2' id='two'>two</option></select></form>";
 
-	st.g("outer").select.selectedIndex = 0;
+	var change = 0,
+		changef = function() {
+			change++;
+		}
 
-	st.bind(st.g("outer").select,"change",changef );
-	
+		st.g("outer").select.selectedIndex = 0;
+
+	st.bind(st.g("outer").select, "change", changef);
+
 	stop()
-	Syn.click( st.g("two"), function(){
-		equals(change, 1 , "change called once")
+	Syn.click(st.g("two"), function() {
+		equals(change, 1, "change called once")
 		equals(st.g("outer").select.selectedIndex, 1, "Change Selected Index");
 		//st.g("qunit-test-area").innerHTML = ""
 		start();
@@ -51,84 +56,81 @@ test("Selecting a select element", function(){
 
 
 
-	
+
 })
 
-asyncTest("scrollTop triggers scroll events", function(){
-	st.g("qunit-test-area").innerHTML = "<div id='scroller' style='height:100px;width: 100px;overflow:auto'>"+
-			"<div style='height: 200px; width: 100%'>text"+
-			"</div>"+
-			"</div>";
-			
-	st.binder("scroller","scroll",function(ev){
-		ok(true,"scrolling created just by changing ScrollTop");
-		st.g("qunit-test-area").innerHTML ="";
+asyncTest("scrollTop triggers scroll events", function() {
+	st.g("qunit-test-area").innerHTML = "<div id='scroller' style='height:100px;width: 100px;overflow:auto'>" + "<div style='height: 200px; width: 100%'>text" + "</div>" + "</div>";
+
+	st.binder("scroller", "scroll", function( ev ) {
+		ok(true, "scrolling created just by changing ScrollTop");
+		st.g("qunit-test-area").innerHTML = "";
 		start();
-	} );
+	});
 	stop();
-	setTimeout(function(){
+	setTimeout(function() {
 		var sc = st.g("scroller");
 		sc && (sc.scrollTop = 10);
-	
-	},13)
-	
+
+	}, 13)
+
 })
 
-test("focus triggers focus events", function(){
+test("focus triggers focus events", function() {
 	st.g("qunit-test-area").innerHTML = "<input type='text' id='focusme'/>";
-	
-	st.binder("focusme","focus",function(ev){
-		ok(true,"focus creates event");
-		st.g("qunit-test-area").innerHTML ="";
+
+	st.binder("focusme", "focus", function( ev ) {
+		ok(true, "focus creates event");
+		st.g("qunit-test-area").innerHTML = "";
 		start();
-	} );
+	});
 	stop();
-	setTimeout(function(){
+	setTimeout(function() {
 		st.g("focusme").focus();
-	
-	},10)
-	
+
+	}, 10)
+
 });
 
 
-test("focus on an element then another in another page", function(){
+test("focus on an element then another in another page", function() {
 	stop(10000);
 	var rootJoin;
-	if(typeof steal == "undefined"){ 
+	if ( typeof steal == "undefined" ) {
 		// hardcoding this path so the standalone synthetic tests will pass
-		rootJoin = function(path){
-			return "../../"+path;
+		rootJoin = function( path ) {
+			return "../../" + path;
 		};
 	} else {
 		rootJoin = $.proxy(steal.root.join, steal.root);
 	}
-	
+
 	var page1 = rootJoin("funcunit/syn/test/qunit/page1.html"),
 		page2 = rootJoin("funcunit/syn/test/qunit/page2.html"),
 		iframe = document.createElement('iframe'),
 		calls = 0;
-	
-	st.bind(iframe,"load",function(){
-		if(calls == 0){
-			
-			Syn.click( iframe.contentWindow.document.getElementById("first") ,{}, function(){
-				
+
+	st.bind(iframe, "load", function() {
+		if ( calls == 0 ) {
+
+			Syn.click(iframe.contentWindow.document.getElementById("first"), {}, function() {
+
 				iframe.contentWindow.location = page2;
 			})
 			calls++;
-		}else{
-			
-			Syn.click( iframe.contentWindow.document.documentElement ,{}, function(){
+		} else {
+
+			Syn.click(iframe.contentWindow.document.documentElement, {}, function() {
 				start();
-				
+
 			})
 		}
-			
-			
+
+
 	});
 	iframe.src = page1
-		
-	
-	
+
+
+
 	st.g("qunit-test-area").appendChild(iframe);
 })
