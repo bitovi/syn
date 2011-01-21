f0843b45a608e5647a5a8383e17f1d7f98462613
Brian Moschel <brian.moschel@gmail.com>
2011-01-21 15:52:38 -0600

    fixing build

--- build.js
+++ build.js
@@ -20,9 +20,7 @@ var copyToDist = function(path){
 	steal.File(path).copyTo("funcunit/syn/resources/"+fileName);
 }
 var filesToCopy = [
-	"funcunit/resources/jquery.js",
-	"jquery/dist/jquery.event.drag.js",
-	"jquery/dist/jquery.event.drop.js"
+	"funcunit/resources/jquery.js"
 ]
 
 for(var i = 0; i < filesToCopy.length; i++) {
