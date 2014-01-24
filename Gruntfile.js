0d5e34a87c48a46dacb152ccf8d9c3aea1dcd033
Alexis Abril <me@alexisabril.com>
2014-01-23 16:16:03 -0800

    Adding stealable files to dist. Issue #26

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,16 @@ module.exports = function (grunt) {
 				command: 'node build.js'
 			}
 		},
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['browsers.js', 'key.js', 'mouse.js', 'syn.js', 'synthetic.js', 'typeable.js', 'drag/drag.js'],
+					dest: 'dist/steal/'
+				}]
+			}
+		},
 		concat: {
 			options: {
 				banner: '/*\n * <%= pkg.title || pkg.name %> - <%= pkg.version %>\n * <%= pkg.homepage %>\n * Copyright (c) <%= new Date().getFullYear() %> <%= pkg.author.name %>\n * <%= new Date().toUTCString() %>\n * Licensed <%= pkg.licenses[0].type %> */\n\n'
@@ -50,6 +60,7 @@ module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-release');
 	grunt.loadNpmTasks('grunt-exec');
 	grunt.loadNpmTasks('testee');
