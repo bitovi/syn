42d44ac31112c47d7bee6883b5c3eb6789db7673
Alexis Abril <me@alexisabril.com>
2017-05-01 11:44:26 -0600

    initial drag files

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
-require('test/syn_test');
-require('test/key_test');
+// require('test/syn_test');
+// require('test/key_test');
 require('test/drag_test');
-require('test/mouse_test');
-require('test/typeable_test');
+// require('test/mouse_test');
+// require('test/typeable_test');
