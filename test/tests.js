acaf09cbe912e10d2a8f343d5a7b4f29edf38765
Kevin Dillon <kdillon@users.noreply.github.com>
2018-11-20 12:45:00 -0500

    Adding tests around a mousebutton event value that was set incorrectly in a previous version of syn. I have no repro on this current version, but adding the tests will be beneficial in locking it down (#166)
    No news is good news?

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@ require('test/key_test');
 require('test/key_regressions_test');
 require('test/mouse_move_test');
 require('test/mouse_button_test');
-//require('test/mouse_regressions_test'); //
+require('test/mouse_regressions_test');
 require('test/drag_test_basic');
 //require('test/drag_test_offsets'); //
 require('test/typeable_test');
